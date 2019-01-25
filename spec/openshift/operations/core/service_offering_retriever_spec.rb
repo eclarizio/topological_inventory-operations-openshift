3b1dd75e34a5c0edd5921fda2c4fbc0b54c29ae2
Erik Clarizio <eclarizio@gmail.com>
2019-01-25 08:05:15 -0800

    Add missing retriever specs

---
+++ spec/openshift/operations/core/service_offering_retriever_spec.rb
@@ -0,0 +1,28 @@
+require "topological_inventory/openshift/operations/core/service_offering_retriever"
+
+module TopologicalInventory
+  module Openshift
+    module Operations
+      module Core
+        describe ServiceOfferingRetriever do
+          let(:subject) { described_class.new(123) }
+
+          describe "#process" do
+            let(:url) { "http://localhost:3000/api/v0.0/service_offerings/123" }
+            let(:headers) { {"Content-Type" => "application/json"} }
+            let(:dummy_response) { {"dummy" => "response"} }
+
+            before do
+              stub_request(:get, url).with(:headers => headers).to_return(:body => dummy_response.to_json)
+            end
+
+            it "returns the service offering response" do
+              expect(subject.process).to eq(dummy_response)
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
