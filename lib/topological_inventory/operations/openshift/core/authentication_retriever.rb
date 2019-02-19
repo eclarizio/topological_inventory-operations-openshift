6e3a8f76f24ed3ddbfae83dd917b65cc6cf236c0
Erik Clarizio <eclarizio@gmail.com>
2019-02-19 10:55:52 -0800

    Use internal api to get the password for an authentication

--- lib/topological_inventory/operations/openshift/core/authentication_retriever.rb
+++ lib/topological_inventory/operations/openshift/core/authentication_retriever.rb
@@ -1,16 +1,22 @@
+require "topological_inventory/operations/openshift/core/retriever"
+
 module TopologicalInventory
   module Operations
     module Openshift
       module Core
-        class AuthenticationRetriever
-          def initialize(endpoint_id)
-            @endpoint_id = endpoint_id
-          end
-
+        class AuthenticationRetriever < Retriever
           def process
-            # Without using Rails example:
-            # https://github.com/ManageIQ/topological_inventory-orchestrator/pull/1/files#diff-4743ee12ee7e621468f2e6590de994efR97
-            Authentication.where(:resource_type => "Endpoint", :resource_id => @endpoint_id).first
+            authentication_id = @api_instance.list_endpoint_authentications(@id.to_s).data[0].id
+            headers = {
+              "Content-Type" => "application/json"
+            }
+            request_options = {
+              :method     => :get,
+              :url        => "#{ENV["TOPOLOGICAL_INVENTORY_URL"]}/#{ENV["PATH_PREFIX"]}/internal/v0.0/authentications/#{authentication_id}?expose_attribute[]=password",
+              :headers    => headers
+            }
+            response = RestClient::Request.new(request_options).execute
+            TopologicalInventoryApiClient::Authentication.new(JSON.parse(response.body))
           end
         end
       end
--- spec/operations/openshift/core/authentication_retriever_spec.rb
+++ spec/operations/openshift/core/authentication_retriever_spec.rb
@@ -5,18 +5,51 @@ module TopologicalInventory
     module Openshift
       module Core
         describe AuthenticationRetriever do
-          let(:tenant) { Tenant.create! }
-          let(:endpoint) { Endpoint.create!(:tenant => tenant) }
-          let(:subject) { described_class.new(endpoint.id) }
+          let(:subject) { described_class.new(123) }
+
+          let(:headers) { {"Content-Type" => "application/json"} }
+          let(:endpoint_authentications_url) do
+            "http://localhost:3000/api/topological-inventory/v0.0/endpoints/123/authentications"
+          end
+          let(:internal_authentications_url) do
+            "http://localhost:3000/api/internal/v0.0/authentications/321?expose_attribute[]=password"
+          end
+          let(:endpoints_authentications_response) do
+            {
+              "data" => [{
+                "id" => 321
+              }]
+            }
+          end
+          let(:internal_authentication_response) { {:password => "token"} }
+
+          before do
+            stub_request(:get, endpoint_authentications_url).with(:headers => headers).to_return(
+              :headers => headers, :body => endpoints_authentications_response.to_json
+            )
+            stub_request(:get, internal_authentications_url).with(:headers => headers).to_return(
+              :headers => headers, :body => internal_authentication_response.to_json
+            )
+          end
+
+          around do |e|
+            url    = ENV["TOPOLOGICAL_INVENTORY_URL"]
+            prefix = ENV["PATH_PREFIX"]
+
+            ENV["TOPOLOGICAL_INVENTORY_URL"] = "http://localhost:3000"
+            ENV["PATH_PREFIX"]               = "api"
+
+            e.run
+
+            ENV["TOPOLOGICAL_INVENTORY_URL"] = url
+            ENV["PATH_PREFIX"]               = prefix
+          end
 
           describe "#process" do
-            let!(:unexpected_authenticaiton) { Authentication.create!(:tenant => tenant) }
-            let!(:expected_authentication) do
-              Authentication.create!(:resource_type => "Endpoint", :resource_id => endpoint.id, :tenant => tenant)
-            end
-
             it "returns the relevant authentication" do
-              expect(subject.process).to eq(expected_authentication)
+              authentication = subject.process
+              expect(authentication.class).to eq(TopologicalInventoryApiClient::Authentication)
+              expect(authentication.password).to eq("token")
             end
           end
         end
